1159b0a26a7ffdca9f38bda4eb04a856babae7c8
VaidehiLodhi <vaidehilodh19@gmail.com>
2025-09-07 12:09:13 +0530

    globe added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
-import type { NextConfig } from "next";
+import { type NextConfig } from "next";
+import path from "path";
+import process from "process";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const pathBuilder = (subpath: string) => path.join(process.cwd(), subpath);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathBuilder("node_modules/cesium/Build/Cesium/Workers"),
+            to: path.join(process.cwd(), "public/cesium/Workers"),
+            info: { minimized: true },
+          },
+          {
+            from: pathBuilder("node_modules/cesium/Build/Cesium/ThirdParty"),
+            to: path.join(process.cwd(), "public/cesium/ThirdParty"),
+            info: { minimized: true },
+          },
+          {
+            from: pathBuilder("node_modules/cesium/Build/Cesium/Assets"),
+            to: path.join(process.cwd(), "public/cesium/Assets"),
+            info: { minimized: true },
+          },
+          {
+            from: pathBuilder("node_modules/cesium/Build/Cesium/Widgets"),
+            to: path.join(process.cwd(), "public/cesium/Widgets"),
+            info: { minimized: true },
+          },
+        ],
+      }),
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify("/cesium"),
+      })
+    );
+
+    return config;
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
